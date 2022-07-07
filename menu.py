4c19234503343a34e0c1a997a355c3fe3b1fd529
camila-PO <camila_oliveira97@hotmail.com>
2022-07-07 15:55:05 -0300

    first commit

--- menu.py
+++ menu.py
@@ -4,59 +4,119 @@ this.opcao = -1
 this.codigo = 0
 this.campo = ""
 
-def menu1():
-    print('\nEscolha um dos exercicios abaixo: \n\n' +
-          '1. Exercìcio para Insegurança.\n'                        +
-          '2. Exercicio para Rotulações.\n'                        +
-          '0.Sair\n')
+def menu():
+    print('\nEscolha uma das opções abaixo: \n\n'                       +
+          '1. Exercìcio para Insegurança.\n'                            +
+          '2. Exercìcio para Rotulação.\n'                              +
+          '3. Consulte suas respostas - Insegurança.\n'+
+          '4. Consulte suas respostas - Rotulação.\n'  +
+          '5. Excluir exercicio - insegurança.\n' +
+          '6. Excluir exercicio - Rotulação.\n'   +
+          '0. Sair\n')
     this.opcao = int(input())
 def operacao():
     while (this.opcao != 0):
-        menu1()
+        menu()
         if this.opcao == 1:
             print('\nOlá, bem vindo(a)! \n\n' +
           'Você fará agora um exercício para te ajudar a duvidar dos seus pensamentos ruins.\n'  +
-          'Por favor, responda as seguintes questões, sendo honesto(a) com você mesmo(a). Faça quantas vezes precisar!\n' +
+          ' \n' +
+          'Por favor, responda as seguintes questões sendo sempre honesto(a) com você mesmo(a).'
+          ''
+          'Repita o exercicio sempre que julgar necessario.\n' +
+          ' \n' +
+          '\033[1;31m *Atenção!! Este exercicio NÃO substitui ajuda psicológica, procure também ajuda profissional.\033[0;0m \n' +
+          ' \n' +
           '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' +
           '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' +
-          'Pergunta 1. Neste momento, o que está te deixando inseguro(a)?\n' +
+          ' \n' +
+          '\033[1;34mPergunta 1. \033[0;0m Neste momento, o que está te deixando inseguro(a)?\n' +
           '\n')
             perg1 = input()
-            print('Pergunta 2. De 0 a 100, qual a chance disso acontecer?')
+            print('\033[1;34mPergunta 2. \033[0;0m De 0 a 100, qual a chance disso acontecer?')
             perg2 = input()
-            print('Pergunta 3. O que prova que esta porcentagem está correta? Evite começar a resposta com "eu acho" ou "eu sinto." \n'
+            print('"\033[1;34m"Pergunta 3.\033[0;0m O que prova que esta porcentagem está correta?'
+                  ' \n' +
+                  'Evite começar a resposta com "eu acho" ou "eu sinto". \n'
                   'Considere somente o que você já tem certeza. E lembre-se, um evento não dá nenhuma certeza a outro, não generalize.')
             perg3 = input()
-            print('Pergunta 4. O que me prova que isso realmente vai acontecer?')
+            print('\033[1;34m Pergunta 4.\033[0;0m O que me prova que isso realmente vai acontecer?')
             perg4 = input()
-            print('Pergunta 5. Por fim, se acolha. Que conselho você daria a um amigo(a) que estivesse passando por esta situação?')
+            print('\033[1;34mPergunta 5.\033[0;0m Por fim, se acolha. Que conselho você daria a um amigo(a) que estivesse passando por esta situação?')
             perg5 = input()
             # Chamar o método inserir
-            operacoes.inserir(perg1, perg2, perg3, perg4, perg5)
-
+            operacoes.inserir1(perg1, perg2, perg3, perg4, perg5)
         elif this.opcao == 2:
             print('\nOlá, bem vindo(a)! \n\n' +
-                  'Você fará agora um exercício para te ajudar a se livrar de rotulações.\n' +
-                  'Por favor, responda as seguintes questões, sendo honesto(a) com você mesmo(a). Faça quantas vezes precisar!\n' +
+                  'Você fará agora um exercício para te ajudar a duvidar dos seus pensamentos ruins.\n' +
+                  ' \n' +
+                  'Por favor, responda as seguintes questões sendo sempre honesto(a) com você mesmo(a).'
+                  ''
+                  'Repita o exercicio sempre que julgar necessario.\n' +
+                  ' \n' +
+                  '\033[1;31m *Atenção!! Este exercicio NÃO substitui ajuda psicológica, procure também ajuda profissional.\033[0;0m \n' +
+                  ' \n' +
                   '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' +
                   '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n' +
-                  'Pergunta 1. O que acho que eu sou? (Descreva com adjetivos).\n' +
+                  ' \n' +
+                  '\033[1;34mPergunta 1. \033[0;0mO que acho que eu sou? (Descreva com adjetivos).\\n' +
                   '\n')
             perg6 = input()
-            print('Pergunta 2. O que significam estas palavras? (Pesquise o significado de cada um dos adjetivos aos quais você se atribuiu.)')
+            print(
+                '\033[1;34mPergunta 2.\033[0;0m O que significam estas palavras? (Pesquise o significado de cada um dos adjetivos aos quais você se atribuiu.)')
             perg7 = input()
             print(
-                'Pergunta 3. Depois desta pesquisa, você realmente acha que se encaixa nestas descrições?')
+                '\033[1;34mPergunta 3.\033[0;0m  Depois desta pesquisa, você realmente acha que se encaixa nestas descrições?')
             perg8 = input()
-            print('Pergunta 4. Se pergunte: Que prova tenho de que isso é real? (Leve em consideração somente FATOS).')
+            print(
+                '\033[1;34mPergunta 4. \033[0;0m Se pergunte: Que prova tenho de que isso é real? (Leve em consideração somente FATOS).')
             perg9 = input()
             print(
-                'Pergunta 5. Por fim, se acolha. Você atribuiria estes adjetivos a um amigo(a) seu?')
+                '\033[1;34mPergunta 5.\033[0;0m Por fim, se acolha. Você atribuiria estes adjetivos a um amigo(a) seu?')
             perg10 = input()
             # Chamar o método inserir
-            operacoes.inserir(perg6, perg7, perg8, perg9, perg10)
-        else:
-            print('Opção escolhida não é válida!')
+            operacoes.inserir2(perg6, perg7, perg8, perg9, perg10)
+
+
+        elif this.opcao == 3:
+          operacoes.consultar1()
+        elif this.opcao == 4:
+            operacoes.consultar2()
+
+        elif this.opcao == 5:
+         print("Informe o código para a exclusão do dado")
+         this.codigo = int(input())
+         operacoes.excluir1(this.codigo)
+
+        elif this.opcao == 6:
+            print("Informe o código para a exclusão do dado")
+            this.codigo = int(input())
+            operacoes.excluir2(this.codigo)
+
+        elif this.opcao == 0:
+            print('Tchau! volte sempre que precisar conversar, você com você mesmo.\033[1;31m♡\033[0;0m \n. ')
+
+    else:
+       print('Opção escolhida não é válida!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- operacoes.py
+++ operacoes.py
@@ -1,40 +1,45 @@
 import mysql.connector
 import conexaoBD #Classe que faz a conexão com o banco de dados
-import menu
+
 db_connection = conexaoBD.conectar()
 con = db_connection.cursor()
 
-def inserir(perg1, perg2, perg3, perg4, perg5):
+def inserir1(perg1, perg2, perg3, perg4, perg5):
     try:
         sql = "insert into inseguranca (codInsegura, perg1, perg2, perg3, perg4, perg5) values('', '{}', '{}','{}', '{}', '{}')".format(perg1, perg2, perg3, perg4, perg5)
         con.execute(sql)
         db_connection.commit() #Inserção de dados no BD
-        print("{} Inserido!".format(con.rowcount))
+        print("	\033[1;37mSuas Respostas foram salvas e estarão sempre disponíveis para que você possa ler e refletir sobre elas.\033[0;0m".format(con.rowcount))
     except Exception as erro:
         return erro
 
 
-def consultar():
+def consultar1():
     try:
         sql = 'select * from inseguranca'
         con.execute(sql)
 
-        for(codigo, perg1, perg2, perg3, perg4, perg5) in con:
-            print('Código: {}, Resposta refente a pergunta 1: {}, Resposta refente a pergunta 2: {}, Resposta refente a pergunta 3 {}, Resposta refente a pergunta 4 {},Resposta refente a pergunta 5 {}, '.format(perg1, perg2, perg3, perg4, perg5))
-        print('\n')
+        for (codInsegura, perg1, perg2, perg3, perg4,perg5) in con:
+            try:
+                sql = 'select * from inseguranca '
+                con.execute(sql)
+
+                for (codInsegura, perg1, perg2, perg3, perg4, perg5) in con:
+                    print(
+                        '\033[1;34m CODIGO DO EXERCICIO:\033[0;0m {}\n\n Naquele momento, o que estava te deixando(a) inseguro(a)?\n\n \033[1;34m Sua resposta:\033[0;0m {}\n\n O que te provava, naquele momento, que esta porcentagem estava correta?\n\n \033[1;34m Sua resposta:\033[0;0m  {}\n\n O que te provava que isso realmente iria acontecer?\n\n \033[1;34m Sua resposta:\033[0;0m  {}\n\n Por fim, Como você se acolheu? Que conselho você daria a um amigo(a) que estivesse passando por esta situação?\n\n \033[1;34m Sua resposta:\033[0;0m  {}'.format(codInsegura,
+                                                                                                          perg1,
+                                                                                                          perg2,
+                                                                                                          perg3,
+                                                                                                          perg4,
+                                                                                                          perg5))
+                print('\n')
+            except Exception as erro:
+                print(erro)
     except Exception as erro:
         print(erro)
 
-def atualizar(cod, campo, novoDado):
-    try:
-        sql = "update inseguranca set {} = '{}' where codInsegura = '{}'".format(campo, novoDado, cod)
-        con.execute(sql)
-        db_connection.commit()
-        print('{} Atualizado!'.format(con.rowcount))
-    except Exception as erro:
-        print(erro)
 
-def excluir(cod):
+def excluir1(cod):
     try:
         sql = "delete from inseguranca where codInsegura = '{}'".format(cod)
         con.execute(sql)
@@ -48,7 +53,7 @@ def inserir2 (perg6, perg7, perg8, perg9, perg10):
         sql = "insert into rotulacao (codRotu, perg6, perg7, perg8, perg9, perg10) values('', '{}', '{}','{}', '{}', '{}')".format(perg6, perg7, perg8, perg9, perg10)
         con.execute(sql)
         db_connection.commit() #Inserção de dados no BD
-        print("{} Inserido!".format(con.rowcount))
+        print("{} \033[1;37mSuas Respostas foram salvas e estarão sempre disponíveis para que você possa ler e refletir sobre elas.\033[0;0m".format(con.rowcount))
     except Exception as erro:
         return erro
 
@@ -59,17 +64,16 @@ def consultar2():
         con.execute(sql)
 
         for(codRotu, perg6, perg7, perg8, perg9, perg10) in con:
-            print('Código: {}, Resposta refente a pergunta 1: {}, Resposta refente a pergunta 2: {}, Resposta refente a pergunta 3 {}, Resposta refente a pergunta 4 {},Resposta refente a pergunta 5 {}, '.format(perg1, perg2, perg3, perg4, perg5))
-        print('\n')
-    except Exception as erro:
-        print(erro)
+            print(
+                '\033[1;34m CODIGO DO EXERCICIO:\033[0;0m {}\n\n Naquele momento, quais adjetivos você se atrubuiu?\n\n \033[1;34m Sua resposta:\033[0;0m {}\n\n De acordo com sua pesquisa, o que significavam estas palavras?\n\n \033[1;34m Sua resposta:\033[0;0m  {}\n\n Depois desta pesquisa, você realmente acha que se encaixa nestas descrições?\n\n \033[1;34m Sua resposta:\033[0;0m  {}\n\n Que fatos você listou como prova de que isso é real? \n\n \033[1;34m Sua resposta:\033[0;0m  {}\n\n Por fim, você atribuiria estas palavras a um amigo(a)?\n\n \033[1;34m Sua resposta:\033[0;0m  {}\n\n'.format(
+                    codRotu,
+                    perg6,
+                    perg7,
+                    perg8,
+                    perg9,
+                    perg10))
+            print('\n')
 
-def atualizar2(cod, campo, novoDado):
-    try:
-        sql = "update rotulacao set {} = '{}' where codRotu = '{}'".format(campo, novoDado, cod)
-        con.execute(sql)
-        db_connection.commit()
-        print('{} Atualizado!'.format(con.rowcount))
     except Exception as erro:
         print(erro)
 
